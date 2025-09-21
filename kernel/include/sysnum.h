49ebd70e69404094e348bcff1d1a39aea9e67b00
姚祥麟 <2498378289@qq.com>
2025-09-21 22:44:09 +0800

    完成getpid

--- kernel/include/sysnum.h
+++ kernel/include/sysnum.h
@@ -3,7 +3,7 @@
 
 // System call numbers
 #define SYS_fork         1
-#define SYS_exit         2
+#define SYS_exit         93
 #define SYS_wait         3
 #define SYS_pipe         4
 #define SYS_read         5
@@ -12,12 +12,12 @@
 #define SYS_fstat        8
 #define SYS_chdir        9
 #define SYS_dup         10
-#define SYS_getpid      11
+#define SYS_getpid      172
 #define SYS_sbrk        12
 #define SYS_sleep       13
 #define SYS_uptime      14
 #define SYS_open        15
-#define SYS_write       16
+#define SYS_write       64
 #define SYS_remove      17
 #define SYS_trace       18
 #define SYS_sysinfo     19
